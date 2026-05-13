33c28f4bb3a486dd5a3769e6673f57d9f16ef5f0
omen <omen@kreator-inc.com>
2026-05-13 16:11:24 +0800

    wip: 暂存当前进度

--- src/app/canvas/page.tsx
+++ src/app/canvas/page.tsx
@@ -14,6 +14,12 @@ export default function CanvasPage() {
         height={600}
         className="bg-white rounded-box shadow-lg"
       />
+      {/* <div className="mockup-phone  border-[#ff8938]">
+        <div className="mockup-phone-camera"></div>
+        <div className="mockup-phone-display">
+          <img alt="wallpaper" src="https://img.daisyui.com/images/stock/453966.webp" />
+        </div>
+      </div> */}
     </div>
   );
 }
